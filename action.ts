3e2ae0338439ad0c2523b3fa534a7f6eecbd02fb
Louis Kamp Eskildsen <louiskampeskildsen99@gmail.com>
2020-05-05 15:11:21 +0200

    ININT

---
+++ action.ts
@@ -0,0 +1,23 @@
+console.log("hi")
+
+import './style.scss'
+import './node_modules/modal-video/css/modal-video.min.css'
+import ModalVideo from 'modal-video';
+
+new ModalVideo('.overlay');
+
+const faq = document.querySelectorAll(".faq-item-head")
+const burger = document.querySelector(".burger")
+const mobileMenu = document.querySelector(".mobile-menu")
+faq.forEach((el) => {
+    const arrow = el.querySelector(".faq-item-head-arrow")
+    const body = el.parentElement.querySelector(".faq-item-body")
+    el.addEventListener('click', () => {
+        body.classList.toggle('faq-item-body-active')
+        arrow.classList.toggle('faq-item-head-arrow-rotate')
+    })
+})
+
+burger.addEventListener('click', () => {
+    mobileMenu.classList.toggle('mobile-menu-active')
+})
